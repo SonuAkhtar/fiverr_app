5c85a7a39ef74b0df5bac756f3390d5926418029
SonuAkhtar <sonua981@gmail.com>
2023-07-22 13:08:35 +0530

    navbar updated

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,11 +6,16 @@ import "./navbar.scss";
 const Navbar = () => {
   const [active, setActive] = useState(false);
   const [menuOpen, setMenuOpen] = useState(false);
+  const [moveDots, setMoveDots] = useState(false);
   const { pathname } = useLocation();
   const navigate = useNavigate();
 
   useEffect(() => {
     window.addEventListener("scroll", isActive);
+
+    setTimeout(() => {
+      setMoveDots(true);
+    }, 1000);
 
     return () => {
       window.removeEventListener("scroll", isActive);
@@ -38,8 +43,10 @@ const Navbar = () => {
       <div className="container">
         <div className="logo">
           <Link to="/" className="link">
-            <span className="text">Fiverr</span>
-            <span className="dot">.</span>
+            Crea
+            <span className={`dot dot1 ${moveDots ? "loaded" : ""}`}></span>
+            <span className="t">T</span>
+            <span className={`dot dot2 ${moveDots ? "loaded" : ""}`}></span>ors
           </Link>
         </div>
         <div className="links">
@@ -54,29 +61,27 @@ const Navbar = () => {
                 alt="user-image"
               />
               <span>{currentUser?.username}</span>
-              {menuOpen && (
-                <div className="options">
-                  {currentUser?.isSeller && (
-                    <>
-                      <Link className="link" to="/myGigs">
-                        Gigs
-                      </Link>
-                      <Link className="link" to="/add">
-                        Add New Gig
-                      </Link>
-                    </>
-                  )}
-                  <Link className="link" to="/orders">
-                    Orders
-                  </Link>
-                  <Link className="link" to="/messages">
-                    Messages
-                  </Link>
-                  <Link className="link" onClick={handleLogout}>
-                    Logout
-                  </Link>
-                </div>
-              )}
+              <div className={`options ${menuOpen ? "open" : ""}`}>
+                {currentUser?.isSeller && (
+                  <>
+                    <Link className="link" to="/myGigs">
+                      Gigs
+                    </Link>
+                    <Link className="link" to="/add">
+                      Add New Gig
+                    </Link>
+                  </>
+                )}
+                <Link className="link" to="/orders">
+                  Orders
+                </Link>
+                <Link className="link" to="/messages">
+                  Messages
+                </Link>
+                <Link className="link" onClick={handleLogout}>
+                  Logout
+                </Link>
+              </div>
             </div>
           ) : (
             <>
